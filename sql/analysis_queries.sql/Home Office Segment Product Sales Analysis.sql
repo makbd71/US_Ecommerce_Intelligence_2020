ffd42530f7b3353be94dd8bc58d6fd456694f9e7
Mohammad Anowarul Kabir <kabir.15@wright.edu>
2026-05-15 06:13:52 -0400

    Add files via upload

---
+++ sql/analysis_queries.sql/Home Office Segment Product Sales Analysis.sql
@@ -0,0 +1,18 @@
+select *from customers limit 10
+
+SELECT  c.segment,
+		oi.product_id,
+		p.product_name,
+		sum(oi.sales) as total_sales,
+		sum(oi.quantity) as total_quantity
+FROM customers c
+JOIN orders o
+    ON c.customer_id = o.customer_id
+JOIN order_items oi
+    ON o.order_id = oi.order_id
+JOIN products p
+	ON p.product_id = oi.product_id 
+WHERE c.segment = 'Home Office'
+Group by 
+c.segment,oi.product_id,p.product_name, oi.sales,oi.quantity
+ORDER BY total_sales DESC;
